46b7240505a85a807eead0579fc861451610b6c5
ramesh02001 <137371718+ramesh02001@users.noreply.github.com>
2024-11-26 22:19:10 +0530

    Update settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*ie5rf%bmykm%!dmj6^^ow&^of-dohsr0cmmx=dixf!x#2!#3h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1:8000",]
+ALLOWED_HOSTS = ["127.0.0.1:8000","djangobackend-2.onrender.com"]
 
 
 # Application definition
