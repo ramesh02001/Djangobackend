4b8340e9d8062cb73cb66a966b4dec41bf39252e
ramesh02001 <137371718+ramesh02001@users.noreply.github.com>
2024-11-27 10:11:22 +0530

    Update settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*ie5rf%bmykm%!dmj6^^ow&^of-dohsr0cmmx=dixf!x#2!#3h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1:8000","djangobackend-2.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1:8000","localhost","djangobackend-2.onrender.com"]
 
 
 # Application definition
@@ -183,6 +183,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
